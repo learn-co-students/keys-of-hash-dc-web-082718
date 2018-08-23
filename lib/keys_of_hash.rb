938134228fb9079f89c660912d9539af8edd8c2b
github username <github email address>
2018-08-23 18:55:20 -0400

    Developed method within Hash class.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    matched_values = Array.new
+    index = 0
+
+
+    arguments.each do |args|
+      each do |key, value|
+        if value == args
+          matched_values[index] = key
+          index += 1
+        end
+      end
+    end
+    matched_values
   end
 end
